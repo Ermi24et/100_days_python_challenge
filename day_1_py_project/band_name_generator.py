622426514e859c3776c73fa0d6fd10f0985ccb5f
Ermi24et <lilermi24@gmail.com>
2023-08-06 16:45:06 +0300

    band name generator

---
+++ day_1_py_project/band_name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city = input("provide the city you grew up in: ")
+pet = input("provide name of a pit: ")
+print("your band name is, " + city + " " + pet )
